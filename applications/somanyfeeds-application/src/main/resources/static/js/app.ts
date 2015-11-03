d434082eba62d62001cca87c08f4d79f12ecc876
Damien Le Berrigaud <damien@itsdamo.com>
2015-11-02 22:06:28 -0700

    Introduce "About" feed.
     * Display about article
     * Handle when no feed is selected.

--- applications/somanyfeeds-application/src/main/resources/static/js/app.ts
+++ applications/somanyfeeds-application/src/main/resources/static/js/app.ts
@@ -12,12 +12,13 @@ module SoManyFeeds {
     }
 
     angular
-        .module("SoManyFeedsApp", ["ngResource", "ngSanitize"])
+        .module("SoManyFeedsApp", ["ngSanitize"])
 
         .value("availableFeeds", [
+            {name: "About", slug: "about", selected: false},
             {name: "Github", slug: "github", selected: false},
-            {name: "Google+", slug: "gplus", selected: true},
-            {name: "Pivotal Blog", slug: "pivotal", selected: true},
+            {name: "Google+", slug: "gplus", selected: false},
+            {name: "Pivotal Blog", slug: "pivotal", selected: false}
         ])
 
         .service("feedPresenter", () => new Feed.Presenter())
@@ -46,21 +47,56 @@ module SoManyFeeds {
             let presentArticle = (article) => articlePresenter.present(article);
             let selectedFeeds = () => availableFeeds.filter(f => f.selected);
 
+            let aboutArticle: Article.ViewModel = {
+                title: "About",
+                link: "http://damo.io",
+                content: document.getElementById("about-article-content").innerHTML,
+                date: "",
+            };
+
             let refreshFeeds = () => {
-                articlesService.getByFeeds(selectedFeeds()).then(function (response) {
-                    let entities: Array<Article.Entity> = response.data.articles;
-                    $scope.articles = entities.map(presentArticle)
-                });
+                let feeds = selectedFeeds();
+                let feedsWithoutAbout = feeds.filter(f => f.slug != "about");
+
+                let displayArticles = (articles: Array<Article.ViewModel>) => {
+                    let shouldDisplayAbout = feeds.some(f => f.slug == "about");
+
+                    if (shouldDisplayAbout) {
+                        articles.unshift(aboutArticle);
+                    }
+
+                    $scope.articles = articles;
+                };
+
+                if (feedsWithoutAbout.length > 0) {
+                    articlesService
+                        .getByFeeds(feedsWithoutAbout)
+                        .then((response) => {
+                            let presentedArticles = response
+                                .data
+                                .articles
+                                .map(presentArticle);
+                            displayArticles(presentedArticles);
+                        });
+                } else {
+                    displayArticles([]);
+                }
+
                 $scope.feeds = availableFeeds.map(presentFeed);
             };
 
             $scope.$on("$locationChangeSuccess", () => {
-                let slugs = $location.path().replace(/^\//, "").split(",");
+                let slugs = $location.path()
+                    .replace(/^\//, "")
+                    .split(",")
+                    .filter(s => s.length > 0);
 
                 availableFeeds.forEach((feed: Feed.Entity) => {
                     feed.selected = slugs.some((s) => s == feed.slug);
                 });
                 refreshFeeds();
             });
+
+            if ($location.path() == "") $location.path("/about,gplus,pivotal")
         });
 }
