11fdef0d40a16db10563790501487df420276869
Damien Le Berrigaud <damien@itsdamo.com>
2016-06-28 18:12:30 -0600

    Enforce time zone when running tests. Simplifying tests for date formatter.

--- applications/somanyfeeds-frontend/tests/elm/SoManyFeedsTests/DateFormat.elm
+++ applications/somanyfeeds-frontend/tests/elm/SoManyFeedsTests/DateFormat.elm
@@ -6,12 +6,8 @@ import SoManyFeeds.DateFormat exposing (parseAndFormat)
 
 tests = suite "DateFormat"
   [
-    test "parseAndFormat formats date" (
-        assert
-          (String.contains "Dec 31 2015 @ " (parseAndFormat "2015-12-31T10:30:59Z"))
-    ),
-    test "parseAndFormat formats time" (
-        assert
-          (String.contains ":30" (parseAndFormat "2015-12-31T10:30:59Z"))
+    test "parseAndFormat" (
+      assert
+        (String.contains "Dec 31 2015 @ 3:30" (parseAndFormat "2015-12-31T10:30:59Z"))
     )
   ]
