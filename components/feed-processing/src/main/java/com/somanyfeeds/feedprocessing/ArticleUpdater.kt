87328ddf4911c929fee7b4b9aa8a95be71716de4
Damien Le Berrigaud <damien@itsdamo.com>
2017-01-01 10:49:59 -0700

    Upgrade to Kotlin 1.0.6
     * Use kotlin-spring plugin configuration of kotlin-all-open.
     * Remove MockMaker files.

--- components/feed-processing/src/main/java/com/somanyfeeds/feedprocessing/ArticleUpdater.kt
+++ components/feed-processing/src/main/java/com/somanyfeeds/feedprocessing/ArticleUpdater.kt
@@ -3,7 +3,9 @@ package com.somanyfeeds.feedprocessing
 import com.somanyfeeds.articledataaccess.Article
 import com.somanyfeeds.articledataaccess.ArticleRepository
 import com.somanyfeeds.feeddataaccess.Feed
+import org.springframework.stereotype.Service
 
+@Service
 class ArticleUpdater(val articleRepo: ArticleRepository,
                      val articleLimit: Int) {
 
