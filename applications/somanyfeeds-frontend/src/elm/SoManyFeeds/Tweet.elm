fb346cb8e1b4efa09db81f4d4d4fdd086aa704cd
Damien Le Berrigaud <damien@itsdamo.com>
2017-01-08 08:38:48 -0700

    Display retweets and mentions.

--- applications/somanyfeeds-frontend/src/elm/SoManyFeeds/Tweet.elm
+++ applications/somanyfeeds-frontend/src/elm/SoManyFeeds/Tweet.elm
@@ -4,14 +4,19 @@ import Html exposing (Attribute, Html, section)
 import Html.Attributes exposing (class)
 import Json.Encode as Encode
 import VirtualDom
+import Regex
 
 
 display : String -> Html msg
 display content =
-    if isRetweet content then
-        section [ class "retweet", innerHtml (retweetContent content) ] []
-    else
-        section [ innerHtml content ] []
+    let
+        tweetContent =
+            createLinks content
+    in
+        if isRetweet content then
+            section [ class "retweet", innerHtml (retweetContent tweetContent) ] []
+        else
+            section [ innerHtml tweetContent ] []
 
 
 isRetweet : String -> Bool
@@ -22,6 +27,35 @@ isRetweet =
 innerHtml : String -> Attribute msg
 innerHtml =
     VirtualDom.property "innerHTML" << Encode.string
+
+
+createLinks : String -> String
+createLinks tweet =
+    let
+        mentionRegex =
+            Regex.regex "(^|\\s)@([A-Za-z_]+[A-Za-z0-9_]+)"
+    in
+        Regex.replace Regex.All mentionRegex mentionLink tweet
+
+
+mentionLink : Regex.Match -> String
+mentionLink match =
+    case match.submatches of
+        maybeLeading :: maybeHandle :: _ ->
+            case maybeHandle of
+                Nothing ->
+                    match.match
+
+                Just submatch ->
+                    (Maybe.withDefault "" maybeLeading)
+                        ++ "<a href=\"https://twitter.com/"
+                        ++ submatch
+                        ++ "\">@"
+                        ++ submatch
+                        ++ "</a>"
+
+        _ ->
+            match.match
 
 
 retweetContent : String -> String
