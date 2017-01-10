e5e54f7c7fbc57a1acbe3bdcfa73f10aff599409
Damien Le Berrigaud <damien@itsdamo.com>
2017-01-10 11:47:19 -0700

    Only get tweets that are neither retweets or replies.

--- components/feed-processing/src/main/java/com/somanyfeeds/feedprocessing/twitter/TwitterFeedProcessor.kt
+++ components/feed-processing/src/main/java/com/somanyfeeds/feedprocessing/twitter/TwitterFeedProcessor.kt
@@ -6,13 +6,17 @@ import com.somanyfeeds.feeddataaccess.FeedType
 import com.somanyfeeds.feedprocessing.FeedProcessor
 import com.somanyfeeds.feedprocessing.toLocalDateTime
 import org.slf4j.LoggerFactory
+import org.springframework.social.twitter.api.Tweet
 import org.springframework.social.twitter.api.Twitter
 import org.springframework.stereotype.Service
 
 @Service
-class TwitterFeedProcessor(val twitter: Twitter) : FeedProcessor {
+class TwitterFeedProcessor(twitter: Twitter) : FeedProcessor {
 
     private val logger = LoggerFactory.getLogger(javaClass)
+    private val pageSize = 50
+    private val tweetsCountToFetch = 20
+    private val timelineOperations = twitter.timelineOperations()
 
 
     override fun canProcess(feed: Feed) = feed.type == FeedType.TWITTER
@@ -20,10 +24,13 @@ class TwitterFeedProcessor(val twitter: Twitter) : FeedProcessor {
     override fun process(feed: Feed): List<Article> {
         logger.debug("Processing Feed: {}", feed)
 
-        val timelineOperations = twitter.timelineOperations()
-        val tweets = timelineOperations.getUserTimeline(feed.info)
+        val tweets = timelineOperations.getUserTimeline(feed.info, pageSize)
 
-        val articles = tweets.map {
+        while (tweets.filtered().count() < tweetsCountToFetch) {
+            tweets.fetchMore(feed.info)
+        }
+
+        val articles = tweets.filtered().map {
             Article(
                 link = "https://twitter.com/${feed.info}",
                 date = it.createdAt.toLocalDateTime(),
@@ -32,7 +39,27 @@ class TwitterFeedProcessor(val twitter: Twitter) : FeedProcessor {
             )
         }
 
-        logger.debug("Processed #{} articles", articles.size)
-        return articles
+        if (logger.isDebugEnabled) {
+            logger.debug("Processed #{} articles", articles.count())
+        }
+
+        return articles.toList()
     }
+
+
+    private fun MutableList<Tweet>.fetchMore(screenName: String) {
+        val maxId: Long = map { it.id }.min()!! - 1
+        val sinceId: Long = 0
+
+        val moreTweets = timelineOperations.getUserTimeline(screenName, pageSize, sinceId, maxId)
+
+        addAll(moreTweets)
+    }
+
+    private fun List<Tweet>.filtered() = asSequence().withoutRetweets().withoutReplies().take(tweetsCountToFetch)
+
+    private fun Sequence<Tweet>.withoutRetweets() = filter { !it.isRetweet }
+
+    private fun Sequence<Tweet>.withoutReplies() = filter { it.inReplyToStatusId == null }
 }
+
